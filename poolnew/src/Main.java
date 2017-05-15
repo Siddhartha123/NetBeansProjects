cc7bf3845ba5e0722e69fe8272db7e5734bd6519
I Siddhartha Kumar <iskumar789@gmail.com>
2017-05-15 17:29:55 +0530

    better design

--- poolnew/src/Main.java
+++ poolnew/src/Main.java
@@ -1,20 +1,29 @@
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        
-        Poolnew p=new Poolnew(3);
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        p.getPoolObject();
-        
-        p.executor.shutdown();
-        /*while (!p.executor.isTerminated()) {
-        }*/
+    public static void main(String[] args)  throws SQLException, ClassNotFoundException, InterruptedException, CloneNotSupportedException {
+        jdbc_con db;
+        db = new jdbc_con("com.mysql.jdbc.Driver","jdbc:mysql://localhost/cyborg","root","");
+        if(db.is_connected){
+            Connection con=db.getConnection(db);
+            Object con1=(Object)con;
+            Poolnew p=new Poolnew(3,con1);
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+            p.getPoolObject();
+
+            //p.executor.shutdown();
+            /*while (!p.executor.isTerminated()) {
+            }*/
+    }
     }
 }
--- poolnew/src/Poolnew.java
+++ poolnew/src/Poolnew.java
@@ -1,38 +1,69 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.Stack;
 
-/**
- *
- * @author I Sidhartha Kumar
- */
 public class Poolnew {
     int n,free,i=0;
     ExecutorService executor;
-    static Vector<Object> ObjectPool = new Vector<Object>();
-    Poolnew(int num){
-        n=num;
-        executor= Executors.newFixedThreadPool(num);
-    }
+    static Stack<Object> ObjectPool = new Stack<Object>();
+    static Vector<Integer> completed=new Vector<>();
+    Queue<Thread> queue;
+    @SuppressWarnings("empty-statement")
     void getPoolObject(){
+        int j;
         i++;
-        Runnable worker = new WorkerThread("p" + i);
+        queue.add(Thread.currentThread());
+        //completed.add(1);
+        while(free==0);
+        
+        Runnable worker = new WorkerThread("p" + i,ObjectPool);
+        //free--;
+        /*
+        for(j=0;j<i-1;j++){
+            while(completed.elementAt(j)==0);
+        }
+        */
         executor.execute(worker);
+        //System.out.println(executor);
+    }
+    
+    Poolnew(int num,Object obj){
+        n=num;
+        free=n;
+        int j;
+        //completed.add(1);
+        executor= Executors.newFixedThreadPool(num);
+        for(j=0;j<n;j++)
+        ObjectPool.add(obj);
+        queue =new LinkedList<>();
     }
     
 }
 
 class WorkerThread implements Runnable{
     private String message;
-    public WorkerThread(String s){
+    Object object;
+    public boolean isComplete=false;
+    public WorkerThread(String s,Stack obj){
+        while(obj.empty());
+        this.object=obj.pop();
         this.message=s;
     }
  
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);
-        System.out.println(Thread.currentThread().getName()+ " running");
+        System.out.println(Thread.currentThread().getName()+" (Start)"+message);
         System.out.println(Thread.currentThread().getName()+" (End)");
+        this.isComplete=true;
+        this.release(object);
+        //p.free++;
+    }
+    public void release(Object object)
+    {
+        Poolnew.ObjectPool.push(object);
     }
  
     
